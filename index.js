d00446175ef9940576c06bbf284c10a86b8c03da
Kench <kenchjustin1@gmail.com>
2026-04-27 02:13:12 +0800

    fix the error

--- index.js
+++ index.js
@@ -25,5 +25,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`🚀 Backend running on port ${PORT}`));
+const PORT = process.env.PORT || 10000; // Use Render's port or 10000
+server.listen(PORT, () => {
+  console.log(`🚀 Backend running on port ${PORT}`);
+});
